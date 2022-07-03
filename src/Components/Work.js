2262ef773ef14cf1ea4c3a0ee283a85ae56ff22d
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-07-03 17:52:07 -0400

    images have links

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -8,8 +8,14 @@ function Work() {
     <>
     <div className='title'>Things I've created</div>
         <div className='my-work-images'>
-            <img className='virtual-vpn' src={img1} alt='virtual-vpn'></img>
-            <img className='yummy' src={img2} alt='yummy'></img>
+            <a href='https://virtual-vpn.herokuapp.com'>
+              <img className='virtual-vpn' src={img1} alt='virtual-vpn'></img>
+              <div className='img-caption'>Virtual-VPN</div>
+            </a>
+            <a href='//yummy-recipes1.herokuapp.com'>
+              <img className='yummy' src={img2} alt='yummy'></img>
+              <div className='img-caption'>Yummy</div>
+            </a>
         </div>
     </>
   )
