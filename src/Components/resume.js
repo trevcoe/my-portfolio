46ed1db6169942401d87403709c0bd153ffbf0b7
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-08-30 13:59:46 -0400

    icon fix...again

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './resume.css';
 import {FaHtml5, FaPython, FaReact, FaCss3, FaFlask} from 'react-icons/fa';
-import {IoLogoJavascript} from 'react-icons/io';
 
 function resume() {
   return (
@@ -17,10 +16,9 @@ function resume() {
         <div className='frameworks'>
             <FaHtml5 className='icon'/>
             <FaCss3 className='icon' />
-            <IoLogoJavascript className='icon' />
+            <FaReact className='icon-2'/>
         </div>
         <div className='frameworks-2'>
-            <FaReact className='icon-2'/>
             <FaFlask className='icon-2'/>
             <FaPython className='icon'/>
         </div>
@@ -40,15 +38,13 @@ function resume() {
     Saved the schools budget by recovering, restoring and recording untracked units.</div>
 
     <div className='title'><b>Education</b></div>
-
         <>
-        <div className='text-2'>August 2021 - June 2022 </div>
-        <div className='text-2'><b>Springboard - Coding Bootcamp Certification</b></div>
-       <div className='text-2'>9-month intensive course in Software Engineering methodologies and technologies. 700+ hours of hands-on course material, with 1:1 industry expert mentor oversight. Mastered skills in front-end web development, back-end web development, databases, data structures and algorithms.</div>
-
         <div className='text-2'>September 2019 - Present</div>
         <div className='text-2'><b>Regent University - Information Systems A.S. - in progress</b></div>
         <div className='text-2'>Relevant courses: Discrete Mathematics, Computer Science Ethics, Information Systems Security.</div>
+        <div className='text-2'>August 2021 - June 2022 </div>
+        <div className='text-2'><b>Springboard - Coding Bootcamp Certification</b></div>
+        <div className='text-2'>9-month intensive course in Software Engineering methodologies and technologies. 700+ hours of hands-on course material, with 1:1 industry expert mentor oversight. Mastered skills in front-end web development, back-end web development, databases, data structures and algorithms.</div>
 
         <div className='text-2'>Graduated June 2017</div>
         <div className='text-2'><b>Chateaugay Central School - High school Diploma</b></div>
